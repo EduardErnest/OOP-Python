130d918e81e3e9cb6fcbe2be1f585d861e3555e0
EduardErnest <edupas001@gmail.com>
2021-04-03 09:59:24 +0100

    Update property.py

--- parent_directory/chapter3/case_study_Agency/property.py
+++ parent_directory/chapter3/case_study_Agency/property.py
@@ -8,7 +8,7 @@ class Property:
 
 
     def display(self):
-        print("PROPERTY DETAILS")
+        print("PROP§ERTY DETAILS")
         print("================")
         print("Square footage: {}".format(self.square_feet))
         print("bedrooms:{}".format(self.num_bedrooms))
@@ -23,3 +23,45 @@ class Property:
 
     prompt_init = staticmethod(prompt_init)                                
 
+class Apartment(Property):
+    valid_laundries = ("coin","ensuite","none")
+    valid_balconies = ("yes","no","solarium")
+    
+    def __init__(self, balcony = '', laundry = '',**kwargs):
+        super().__init__(**kwargs)
+        self.balcony = balcony
+        self.laundry = laundry
+
+
+    def display(self):
+        super().display()
+        print("Apartment details")
+        print("laundry: %s" %self.laundry)
+        print("has balcony: %s" %self.balcony)
+
+    def prompt_init():
+
+        parent_init = Property.prompt_init()
+        laundry = ''
+        while laundry.lower() not in \
+            Apartment.valid_laundries:
+            laundry = input("What laundry facilities does"
+                            "the propert have? ({})".format (
+                                ', '.join(Apartment.valid_laundries)
+                            ))
+        balcony = ''
+
+        while balcony.lower() not in \
+            Apartment.valid_balconies:
+            balcony = input(
+                "Does the property have a balcony? "
+                "({})".format (
+                    " ,".join(Apartment.valid_balconies)
+                )
+            )
+        parent_init.update({
+            "laundry": laundry,
+            "balcony": balcony
+        })    
+        return parent_init
+    prompt_init = staticmethod(prompt_init)   
